3b04ca40da8495fa06910cc283e74dbd9f4da390
tonouchi <y.tonouchi.sys23@morijyobi.ac.jp>
2023-11-22 10:40:23 +0900

    Bmi.javaの編集

--- Bmi.java
+++ Bmi.java
@@ -4,16 +4,19 @@ public class Bmi {
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
 
-        System.out.print("・身長(m)を入力してください：");
-        double height = stdIn.nextDouble();
-        System.out.print("・体重(Kg)を入力してください：");
-        double weight = stdIn.nextDouble();
+        for (int i = 1; i <= 2; i++) {
+            System.out.println("Person " + i + ":");
+            System.out.print("・身長(m)を入力してください：");
+            double height = stdIn.nextDouble();
+            System.out.print("・体重(Kg)を入力してください：");
+            double weight = stdIn.nextDouble();
 
-        double bmi = weight / Math.pow(height, 2);
-        
-        String formattedBmi = String.format("%.2f", bmi);
+            double bmi = weight / Math.pow(height, 2);
 
-        System.out.println("------------------------------");
-        System.out.println("BMIは" + formattedBmi + "です。");
+            String formattedBmi = String.format("%.2f", bmi);
+
+            System.out.println("------------------------------");
+            System.out.println("BMIは" + formattedBmi + "です。");
+        }
     }
 }
